f1d60eb52ba02596f3a66434f76c53840a44c872
Mahdavi3649 <ivadham.smay3649@gmail.com>
2022-05-24 14:50:53 +0200

    Fatto milestone 1!  -- con l’utilizzo di PHP

---
+++ php/index.php
@@ -0,0 +1,125 @@
+<?php 
+
+$dischi = [
+        [
+            "poster" => "https://www.onstageweb.com/wp-content/uploads/2018/09/bon-jovi-new-jersey.jpg",
+            "title"=> "New Jersey",
+            "author"=> "Bon Jovi",
+            "genre"=> "Rock",
+            "year"=> "1988"
+        ],
+        [
+            "poster"=> "https://img.discogs.com/vknPDdrqRbT92pNRX0W4I5N91jg=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1246953-1448927086-6590.jpeg.jpg",
+            "title"=> "Live at Wembley 86",
+            "author"=> "Queen",
+            "genre"=> "Pop",
+            "year"=> "1992"
+        ],
+        [
+            "poster"=> "https://images-na.ssl-images-amazon.com/images/I/41JD3CW65HL.jpg",
+            "title"=> "Ten's Summoner's Tales",
+            "author"=> "Sting",
+            "genre"=> "Pop",
+            "year"=> "1993"
+        ],
+        [
+            "poster"=> "https://cdn2.jazztimes.com/2018/05/SteveGadd-800x723.jpg",
+            "title"=> "Steve Gadd Band",
+            "author"=> "Steve Gadd Band",
+            "genre"=> "Jazz",
+            "year"=> "2018"
+        ],
+        [
+            "poster"=> "https://images-na.ssl-images-amazon.com/images/I/810nSIQOLiL._SY355_.jpg",
+            "title"=>"Brave new World",
+            "author"=> "Iron Maiden",
+            "genre"=> "Metal",
+            "year"=> "2000"
+        ],
+        [
+            "poster"=> "https://upload.wikimedia.org/wikipedia/en/9/97/Eric_Clapton_OMCOMR.jpg",
+            "title"=> "One more car, one more raider",
+            "author"=> "Eric Clapton",
+            "genre"=> "Rock",
+            "year"=>"2002"
+        ],
+        [
+            "poster"=> "https://images-na.ssl-images-amazon.com/images/I/51rggtPgmRL.jpg",
+            "title"=>"Made in Japan",
+            "author"=> "Deep Purple",
+            "genre"=>"Rock",
+            "year"=>"1972"
+        ],
+        [
+            "poster"=> "https://images-na.ssl-images-amazon.com/images/I/81r3FVfNG3L._SY355_.jpg",
+            "title"=>"And Justice for All",
+            "author"=> "Metallica",
+            "genre"=> "Metal",
+            "year"=>"1988"
+        ],
+        [
+            "poster"=> "https://img.discogs.com/KOBsqQwKiNKH-q927oHMyVdDzSo=/fit-in/596x596/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-6406665-1418464475-6120.jpeg.jpg",
+            "title"=> "Hard Wired",
+            "author"=>"Dave Weckl",
+            "genre"=> "Jazz",
+            "year"=> "1994"
+        ],
+        [
+            "poster"=> "https://m.media-amazon.com/images/I/71K9CbNZPsL._SS500_.jpg",
+            "title"=> "Bad",
+            "author"=> "Michael Jacjson",
+            "genre"=>"Pop",
+            "year"=> "1987"
+        ]
+
+    ];
+
+//var_dump($dischi);
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css' rel='stylesheet' integrity='sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3' crossorigin='anonymous'>
+    <link rel="stylesheet" href="./style.css">
+    <title>php ajax dischi</title>
+</head>
+<body>
+
+    <header>
+       <div class="logo">
+          <img src="./img/logo.png"/>
+      </div>
+    </header>
+    <main>
+        <div class="container">
+            <div class="row p-5 row-cols-5 row-cols-sm-3">
+                <?php foreach ($dischi as $disco) { ?>
+                    
+                    <div class="col-2-5">
+                        <img class="img-disco"src="<?php echo $disco["poster"];?>" />
+                        <h3 class="fs-6 text-center text-white fw-bold mt-3 text-uppercase">
+                           <?php echo $disco["title"]?>
+                        </h3>
+                        <div class="caption mt-3"><?php echo $disco["author"]?></div>
+                        <div class="caption"><?php echo $disco["year"]?></div>
+                    </div>
+
+                 <?php } ?> 
+                    
+            </div>
+        </div>
+
+
+    </main>
+    
+
+    
+</body>
+</html>
